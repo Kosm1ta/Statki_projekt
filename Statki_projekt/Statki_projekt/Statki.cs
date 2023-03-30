e892e823e46552e37d7b9dace9652eff8d844150
Kosmita <mazany.piotr@gmail.com>
2023-03-30 16:00:18 +0200

    [UPDATE] Tablice w statkach

--- Statki_projekt/Statki_projekt/Statki.cs
+++ Statki_projekt/Statki_projekt/Statki.cs
@@ -8,5 +8,16 @@ namespace Statki_projekt
 {
     internal class Statki
     {
+        int[] statek1wsp = new int[2];
+        int[] statek2wsp = new int[4];
+        int[] statek3wsp = new int[6];
+
+        public Statki() { }
+        public Statki(int[] statek1wsp, int[] statek2wsp, int[] statek3wsp)
+        { 
+            this.statek1wsp = statek1wsp;
+            this.statek2wsp = statek2wsp;
+            this.statek3wsp = statek3wsp;
+        }
     }
 }
